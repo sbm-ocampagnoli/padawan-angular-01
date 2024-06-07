e07e61ab73bee0cce441ee65e483c59ff5a88a2b
sbm-ocampagnoli <otavio.campagnoli@sbmtechnology.com.br>
2024-06-07 09:43:53 -0300

    Adicionando router e listagem de pensamentos.

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  pensamentos = [
+    {
+      conteudo: 'Terminei! Só falta testar',
+      autoria: 'Dev Júnior',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Se não tem problema, solucionado está.',
+      autoria: 'Dev Sênior',
+      modelo: 'modelo2'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
