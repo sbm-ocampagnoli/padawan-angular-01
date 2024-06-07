9e42ce0cd3da8a4ba7e18606d229fde0427acd79
sbm-ocampagnoli <otavio.campagnoli@sbmtechnology.com.br>
2024-06-07 14:15:07 -0300

    Adicionando exclusão do pensamento.

--- memoteca/src/app/app-routing.module.ts
+++ memoteca/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensam
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
+import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
 
 const routes: Routes = [
   {
@@ -10,12 +11,16 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path:'criarPensamento',
+    path: 'criarPensamento',
     component: CriarPensamentoComponent
   },
   {
     path: 'listarPensamento',
     component: ListarPensamentoComponent
+  },
+  {
+    path: 'pensamentos/excluirPensamento/:id',
+    component: ExcluirPensamentoComponent
   }
 ];
 
